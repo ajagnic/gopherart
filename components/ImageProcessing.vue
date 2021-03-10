1caabc3b51490c1b9d1628263b0b1a6f50e1981e
ajagnic <adrian.agnic@hotmail.com>
2021-03-10 16:23:27 -0500

    can read file

--- components/ImageProcessing.vue
+++ components/ImageProcessing.vue
@@ -4,13 +4,31 @@
     <v-card-subtitle>Upload your own image to process</v-card-subtitle>
     <v-card-text>
       <v-file-input
+        v-model="input"
         hint="*.jpeg, *.png"
         persistent-hint
         accept="image/jpeg, image/png"
       />
     </v-card-text>
     <v-card-actions>
-      <v-btn x-large block plain>Submit</v-btn>
+      <v-btn x-large block plain @click="test">Submit</v-btn>
     </v-card-actions>
   </v-card>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    input: null,
+  }),
+
+  methods: {
+    test() {
+      this.input.arrayBuffer().then((result) => {
+        const arr = new Int8Array(result)
+        console.log(arr)
+      })
+    },
+  },
+}
+</script>
