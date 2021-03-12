0eb88184f18f9cc640b690693fa89ae0bf4fbb2e
ajagnic <adrian.agnic@hotmail.com>
2021-03-12 16:00:52 -0500

    initial image positioning; button controls

--- components/ImageProcessing.vue
+++ components/ImageProcessing.vue
@@ -1,19 +1,34 @@
 <template>
   <v-card>
-    <v-card-title>Generate Image</v-card-title>
-    <v-card-subtitle>Upload your own image to process</v-card-subtitle>
-    <v-card-text>
-      <v-file-input
-        v-model="input"
-        hint="*.jpeg, *.png"
-        persistent-hint
-        accept="image/jpeg, image/png"
-      />
-    </v-card-text>
-    <v-card-actions>
-      <v-btn x-large block plain @click="processFile">Submit</v-btn>
-    </v-card-actions>
-    <img v-if="dataURL != null" :src="dataURL" />
+    <div v-if="dataURL == null">
+      <v-card-title>Generate Image</v-card-title>
+      <v-card-subtitle>Upload your own image to process</v-card-subtitle>
+      <v-card-text>
+        <v-file-input
+          v-model="input"
+          hint="*.jpeg, *.png"
+          persistent-hint
+          accept="image/jpeg, image/png"
+        />
+      </v-card-text>
+      <v-card-actions>
+        <v-btn x-large block plain @click="processFile">Submit</v-btn>
+      </v-card-actions>
+    </div>
+    <div v-if="dataURL != null">
+      <v-card-actions>
+        <v-btn>Advanced</v-btn>
+        <v-spacer />
+        <v-btn @click="processFile">Generate</v-btn>
+      </v-card-actions>
+      <v-card-text>
+        <v-row>
+          <v-spacer />
+          <img width="90%" :src="dataURL" />
+          <v-spacer />
+        </v-row>
+      </v-card-text>
+    </div>
   </v-card>
 </template>
 
