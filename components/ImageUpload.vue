75366bcee8f8c37ebf83f334a738d6848d97c683
ajagnic <adrian.agnic@hotmail.com>
2021-03-18 09:45:33 -0400

    show alert + block submit on error

--- components/ImageUpload.vue
+++ components/ImageUpload.vue
@@ -15,19 +15,38 @@
     <v-card-title v-if="!fileDropped" class="justify-center">
       Drop a file, or&nbsp;
       <label>
-        <v-file-input v-show="false" v-model="file" @change="onUpload" />
+        <v-file-input
+          v-show="false"
+          v-model="file"
+          accept="image/jpeg, image/png"
+          @change="onUpload"
+        />
         <a>select one.</a>
       </label>
     </v-card-title>
-    <v-card-title v-else class="justify-center font-italic">{{
-      filename
-    }}</v-card-title>
+    <v-alert
+      v-if="error"
+      class="mx-auto"
+      type="error"
+      max-width="400"
+      dismissible
+    >
+      {{ status }}
+    </v-alert>
+    <v-card-title
+      v-if="fileDropped && !error"
+      class="justify-center font-italic"
+    >
+      {{ filename }}
+    </v-card-title>
   </v-card>
 </template>
 
 <script>
 export default {
   data: () => ({
+    error: false,
+    status: null,
     file: null,
     filename: null,
     fileDropped: false,
@@ -38,20 +57,43 @@ export default {
   }),
 
   methods: {
-    showUploaded() {
+    showError(status) {
+      this.file = null
+      this.fileDropped = false
+      this.error = true
+      this.iconColor = 'error'
+      this.iconStyle = {
+        'padding-top': '30px',
+      }
+      this.status = status
+    },
+
+    showSuccess() {
       this.fileDropped = true
+      this.error = false
       this.iconColor = 'primary'
       this.iconStyle = {}
+      this.filename = this.file.name
     },
 
     onDrop(e) {
+      const fileList = e.dataTransfer.files
       this.file = e.dataTransfer.files[0]
+      if (fileList.length > 1) {
+        this.showError('Please select a single file.')
+      }
       this.onUpload()
     },
 
     onUpload() {
-      this.showUploaded()
-      this.filename = this.file.name
+      if (this.file != null) {
+        const type = this.file.type
+        if (type !== 'image/jpeg' && type !== 'image/png') {
+          this.showError('Please select a JPEG or PNG image.')
+        } else {
+          this.showSuccess()
+        }
+      }
       this.$emit('file-upload', this.file)
     },
   },
