9d4bd3070c97d37001affa027aa93fd7e6183413
ajagnic <adrian.agnic@hotmail.com>
2021-03-23 05:01:53 -0400

    loading overlay; minor wording changes

---
+++ components/Loading.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-overlay v-if="loading" opacity="0.6">
+    <p color="primary">Loading...</p>
+  </v-overlay>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    loading: false,
+  }),
+
+  methods: {
+    start() {
+      this.loading = true
+    },
+
+    finish() {
+      this.loading = false
+    },
+  },
+}
+</script>
