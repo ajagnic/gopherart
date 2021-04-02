0de7d5727e4541e7e3e068b323b40f7ed38b69cf
ajagnic <adrian.agnic@hotmail.com>
2021-04-01 23:03:42 -0400

    instantiatestreaming pf for safari

--- static/js/go_worker.js
+++ static/js/go_worker.js
@@ -1,6 +1,13 @@
 self.importScripts('wasm_exec.js')
 
 const go = new Go()
+
+if (!WebAssembly.instantiateStreaming) {
+  WebAssembly.instantiateStreaming = async (resp, importObject) => {
+    const source = await (await resp).arrayBuffer()
+    return await WebAssembly.instantiate(source, importObject)
+  }
+}
 WebAssembly.instantiateStreaming(fetch('main.wasm'), go.importObject).then(
   (result) => {
     go.run(result.instance)
